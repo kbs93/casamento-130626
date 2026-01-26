1443820e8960d7dd6b1048c1f22d7f5f20caf895
kbs93 <kbswebb@gmail.com>
2026-01-25 22:34:47 -0300

    ATUALIZAÇÃO e adicinando novos codigos

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBe4j1o-LgZfPhN2Jix1y3OVnNykPv-aPk",
+    authDomain: "casamento-130626.firebaseapp.com",
+    projectId: "casamento-130626",
+    storageBucket: "casamento-130626.firebasestorage.app",
+    messagingSenderId: "860497662347",
+    appId: "1:860497662347:web:1d3f0db4c467b806099f8e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
